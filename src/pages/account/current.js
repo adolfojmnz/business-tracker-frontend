5c18f02380e4827045c33ca1dbaacec38c384db7
Adolfo Jimenez <adolfoj@protonmail.com>
2024-01-11 18:07:57 -0400

    Implement Login and Register functionalies

---
+++ src/pages/account/current.js
@@ -0,0 +1,5 @@
+import CurrentAccount from "@/app/components/account/current";
+
+export default function profileCurrentUserPage() {
+  return <CurrentAccount />;
+}
---
+++ src/pages/account/login.js
@@ -0,0 +1,5 @@
+import Login from "@/app/components/account/login";
+
+export default function LoginPage() {
+  return <Login />;
+}
